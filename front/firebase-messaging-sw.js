cfa8458ee33a0a337614f9d7b25d950a61abbda0
kuhaku-space <kuhakuspace2000@gmail.com>
2024-10-23 16:15:47 +0900

    Add front files

---
+++ front/firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('push', function (event) {
+    // メッセージを表示する
+    var data = {};
+    if (event.data) {
+        data = event.data.json();
+    }
+    var title = data.notification.title;
+    var message = data.notification.body;
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            'body': message
+        })
+    );
+});
